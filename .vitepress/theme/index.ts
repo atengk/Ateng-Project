e220035712d4db18c95d88cce47f1d9214af9710
孔余 <2385569970@qq.com>
2026-03-27 22:29:47 +0800

    feat: markdown图片放大的支持

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,11 +1,35 @@
-// .vitepress/theme/index.ts
-import type { Theme } from 'vitepress'
+import { useRoute } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-// @ts-ignore
-import Layout from './Layout.vue'
+import { nextTick, onMounted, watch } from 'vue'
+import mediumZoom from 'medium-zoom'
 import './index.css'
 
+let zoom: ReturnType<typeof mediumZoom> | null = null
+
 export default {
-    extends: DefaultTheme,
-    Layout
-} satisfies Theme
+    ...DefaultTheme,
+
+    setup() {
+        const route = useRoute()
+
+        const init = () => {
+            if (!zoom) {
+                // @ts-ignore
+                zoom = mediumZoom({
+                    background: 'rgba(0,0,0,0.85)',
+                    margin: window.innerWidth < 768 ? 0 : 24,
+                    scrollOffset: 0,
+                    animationDuration: window.innerWidth < 768 ? 200 : 260
+                })
+            }
+
+            zoom.detach()
+            zoom.attach('.vp-doc img:not(.no-zoom)')
+        }
+
+        const run = () => requestAnimationFrame(() => nextTick(init))
+
+        onMounted(run)
+        watch(() => route.path, run)
+    }
+}
